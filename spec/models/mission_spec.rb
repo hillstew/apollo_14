cac8704b526fc11b7a664bf8798b4899759d8211
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-11 10:37:42 -0600

    Add test for self.sort_a for Mission model

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -10,4 +10,21 @@ describe Mission, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :astronauts}
   end
+
+  describe "Class Methods" do
+    describe "self.sort_a" do
+      it "should return missions alphabetically" do
+        Mission.create(title: "z", time_in_space: 2)
+        Mission.create(title: "g", time_in_space: 2)
+        Mission.create(title: "b", time_in_space: 2)
+        Mission.create(title: "f", time_in_space: 2)
+
+        expect("b").to eq(Mission.sort_a.first.title)
+
+        Mission.create(title: "a", time_in_space: 2)
+        
+        expect("a").to eq(Mission.sort_a.first.title)
+      end
+    end
+  end
 end
