1fd09b01ea3a02d2e06e0e8020a74748ba99951a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-11 10:38:02 -0600

    Add self.sort_a method in Mission model

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,4 +3,8 @@ class Mission < ApplicationRecord
   has_many :astronauts, through: :astronaut_missions
 
   validates_presence_of :title, :time_in_space
+
+  def self.sort_a
+    order(:title)
+  end
 end
